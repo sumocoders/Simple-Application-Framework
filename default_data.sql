3d04b4a24b333e0ebdb454ff4329e23e666fa3eb
Tijs Verkoyen <tijs@verkoyen.eu>
2013-04-17 09:01:31 +0200

    Reset blocked_on-date

--- default_data.sql
+++ default_data.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
 
 INSERT INTO `users` (`id`, `name`, `password`, `email`, `secret`, `type`, `data`, `blocked`, `created_on`, `edited_on`, `blocked_on`) VALUES
-(1, 'Sumocoders', '57bf7fd2449834188ee1497644294a27c93f0faa', 'accounts@sumocoders.be', 'b6b8968b9ffc307e727fe3a437d967db', 'admin', 'a:1:{s:8:"settings";a:0:{}}', 'N', '2012-06-20 13:37:00', NULL, '0000-00-00 00:00:00');
+(1, 'Sumocoders', '57bf7fd2449834188ee1497644294a27c93f0faa', 'accounts@sumocoders.be', 'b6b8968b9ffc307e727fe3a437d967db', 'admin', 'a:1:{s:8:"settings";a:0:{}}', 'N', '2012-06-20 13:37:00', NULL, NULL);
 
 CREATE TABLE IF NOT EXISTS `users_sessions` (
   `session_id` varchar(255) NOT NULL,
