2cf0668b91ae916acc66252b28152bda0f817ca7
Tijs Verkoyen <tijs@verkoyen.eu>
2013-06-02 22:11:08 +0200

    Cleanup

--- modules/example/actions/index.php
+++ modules/example/actions/index.php
@@ -13,24 +13,9 @@ class ExampleIndex extends SiteBaseAction
 {
 	/**
 	 * Execute the action
-	 *
-	 * @return void
 	 */
 	public function execute()
 	{
-		// parse
-		$this->parse();
-
-		// display the page
 		$this->display();
-	}
-
-	/**
-	 * Parse the page
-	 *
-	 * @return void
-	 */
-	private function parse()
-	{
 	}
 }
