5492aa627b3afc1e787aed6db01af557081c9445
Tijs Verkoyen <tijs@verkoyen.eu>
2013-08-06 15:16:33 +0200

    Add title for icons, so we have a tooltip

--- core/classes/datagrid.php
+++ core/classes/datagrid.php
@@ -100,7 +100,7 @@ class SiteDataGrid extends SpoonDataGrid
 			$class .= ' action';
 
 			// rebuild value
-			$html = '<a class="iconLink" href="' . $URL . '"';
+			$html = '<a class="iconLink" href="' . $URL . '" title="' . $value . '"';
 
 			// if the action name contains delete we should ask confirmation
 			if(substr_count($name, 'delete'))
